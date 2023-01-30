f5d7e51350eb9180289d8b02e32bc70621a7478f
Rahul7044 <rahulkumar581999@gmail.com>
2023-01-30 14:36:38 +0530

    elementsByTagName

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-var items = document.getElementsByClassName('list-group-item');
-items[2].style.backgroundColor='green';
+// var items = document.getElementsByClassName('list-group-item');
+// items[2].style.backgroundColor='green';
 
-for(var i = 0; i < items.length; i++){
-    items[i].style.fontWeight='bold';
+// for(var i = 0; i < items.length; i++){
+//     items[i].style.fontWeight='bold';
+// }
+//var news = document.getElementsByClassName('new');
+//news.style.fontWeight='bold';
+
+var li = document.getElementsByTagName('li');
+console.log(li[2]);
+
+li[1].textContent='Rahul';
+li[4].textContent='kumar';
+//li[4].style.fontWeight='bold';
+li[4].style.backgroundColor='red';
+console.log(li[4]);
+
+
+
+for(var i = 0; i<li.length; i++){
+    li[i].style.fontWeight='bold';
 }
